cfd5824ea515f043d461c1635e0e1dc5f089d629
killerisl <killmef@sina.com>
2018-03-30 15:48:22 +0800

    Create navigator.js

---
+++ navigator.js
@@ -0,0 +1,4 @@
+this.userAgent = window.navigator.userAgent
+      if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(this.userAgent)) {
+          window.location.href = "http://www.jlwlxd.com/m/";
+      }
